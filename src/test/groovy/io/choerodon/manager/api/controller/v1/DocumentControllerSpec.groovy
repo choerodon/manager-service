c096bd2a62174c98a0d28ecd9f0d763e19584dee
Eugen <shiying.shen@hand-china.com>
2018-08-28 11:21:49 +0800

    [ADD]add test:config-controller

---
+++ src/test/groovy/io/choerodon/manager/api/controller/v1/DocumentControllerSpec.groovy
@@ -0,0 +1,11 @@
+package io.choerodon.manager.api.controller.v1
+
+import spock.lang.Specification
+
+class DocumentControllerSpec extends Specification {
+    def "Get"() {
+    }
+
+    def "Refresh"() {
+    }
+}
