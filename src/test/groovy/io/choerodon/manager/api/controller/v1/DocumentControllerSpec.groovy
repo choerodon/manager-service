bf2cda20d33e58f2f63c3fa4fcfbe5f23ea93aad
Eugen <shiying.shen@hand-china.com>
2018-08-28 21:06:49 +0800

    [ADD]test:config service

--- src/test/groovy/io/choerodon/manager/api/controller/v1/DocumentControllerSpec.groovy
+++ src/test/groovy/io/choerodon/manager/api/controller/v1/DocumentControllerSpec.groovy
@@ -1,9 +1,30 @@
 package io.choerodon.manager.api.controller.v1
 
+import io.choerodon.manager.IntegrationTestConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.annotation.Import
 import spock.lang.Specification
+import spock.lang.Stepwise
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+/**
+ * @author Eugen
+ */
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Import(IntegrationTestConfiguration)
+@Stepwise
 class DocumentControllerSpec extends Specification {
+    @Autowired
+    private TestRestTemplate restTemplate
+
     def "Get"() {
+//        when: '对【获取服务id对应的版本swagger json字符串】接口发送请求'
+//        def entity = restTemplate.getForEntity('/docs/' + 'iam' + '?version=' + null, String)
+//        then: '查看接口返回值'
+//        entity.statusCode.is2xxSuccessful()
     }
 
     def "Refresh"() {
