172e160857059df3ddfb83b9d79f991e9f4ac2ed
Eugen <shiying.shen@hand-china.com>
2018-08-30 15:41:44 +0800

    [IMP]add mock

--- src/test/groovy/io/choerodon/manager/api/controller/v1/ApiControllerSpec.groovy
+++ src/test/groovy/io/choerodon/manager/api/controller/v1/ApiControllerSpec.groovy
@@ -1,6 +1,8 @@
 package io.choerodon.manager.api.controller.v1
 
 import io.choerodon.manager.IntegrationTestConfiguration
+import io.choerodon.manager.domain.manager.entity.RouteE
+import io.choerodon.manager.domain.repository.RouteRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -15,12 +17,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(IntegrationTestConfiguration)
 class ApiControllerSpec extends Specification {
-
+    @Autowired
+    RouteRepository routeRepository
     @Autowired
     private TestRestTemplate restTemplate
 
     def "Resources"() {
-
+        given: 'mock RouteRepository的getAllRoute()'
+        routeRepository.getAllRoute() >> new ArrayList<RouteE>()
         when: "发送一个get请求"
         def entity = restTemplate.getForEntity("/v1/swaggers/resources", String)
 
