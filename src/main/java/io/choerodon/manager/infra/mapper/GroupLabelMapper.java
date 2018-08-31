735eb704345c621c8f4c53da7310e039d2b0e858
Eugen <shiying.shen@hand-china.com>
2018-08-31 15:28:20 +0800

    [DEL]删除了过时的文件

--- src/main/java/io/choerodon/manager/infra/mapper/GroupLabelMapper.java
+++
@@ -1,7 +0,0 @@
-package io.choerodon.manager.infra.mapper;
-
-import io.choerodon.manager.infra.dataobject.GroupLabelDO;
-import io.choerodon.mybatis.common.BaseMapper;
-
-public interface GroupLabelMapper extends BaseMapper<GroupLabelDO> {
-}
