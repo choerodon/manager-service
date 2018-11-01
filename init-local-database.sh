2eaac99a8c788f3c220bd266c43914e9388e9834
superlee <superleader8@gmail.com>
2018-11-01 11:56:40 +0800

    [ADD] skywalking agent测试

--- init-local-database.sh
+++ init-local-database.sh
@@ -4,10 +4,10 @@ if [ ! -f target/choerodon-tool-liquibase.jar ]
 then
     curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.5.2.RELEASE/choerodon-tool-liquibase-0.5.2.RELEASE.jar -L  -o target/choerodon-tool-liquibase.jar
 fi
-#java -Dspring.datasource.url="jdbc:mysql://localhost/manager_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
-java -Dspring.datasource.url="jdbc:oracle:thin:@127.0.0.1:1521:xe" \
- -Dspring.datasource.username=manager_service \
- -Dspring.datasource.password=123456 \
+#java -Dspring.datasource.url="jdbc:oracle:thin:@127.0.0.1:1521:xe" \
+java -Dspring.datasource.url="jdbc:mysql://localhost/manager_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
+ -Dspring.datasource.username=root \
+ -Dspring.datasource.password=root \
  -Ddata.drop=false -Ddata.init=true \
  -Ddata.dir=src/main/resources \
  -jar target/choerodon-tool-liquibase.jar
