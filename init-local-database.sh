7e6626542bc32cf6cc59ce7acf1a2d3839588ef2
superlee <superleader8@gmail.com>
2018-11-07 19:57:07 +0800

    [IMP] 修改skywalking的collector地址

--- init-local-database.sh
+++ init-local-database.sh
@@ -2,7 +2,7 @@
 mkdir -p target
 if [ ! -f target/choerodon-tool-liquibase.jar ]
 then
-    curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.5.2.RELEASE/choerodon-tool-liquibase-0.5.2.RELEASE.jar -L  -o target/choerodon-tool-liquibase.jar
+    curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.6.3.RELEASE/choerodon-tool-liquibase-0.6.3.RELEASE.jar -L  -o target/choerodon-tool-liquibase.jar
 fi
 #java -Dspring.datasource.url="jdbc:oracle:thin:@127.0.0.1:1521:xe" \
 java -Dspring.datasource.url="jdbc:mysql://localhost/manager_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
