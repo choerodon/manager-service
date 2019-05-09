2040c50b009d60038ab71570f8e4099c92e998b6
superlee <superleader8@gmail.com>
2019-05-09 19:55:59 +0800

    [REF] 前后端合并，后段代码修改

---
+++ react/config.js
@@ -0,0 +1,11 @@
+const config = {
+  // server: 'http://api.staging.saas.hand-china.com',
+  server: 'http://api.c7nf.choerodon.staging.saas.hand-china.com',
+  master: '@choerodon/master',
+  projectType: 'choerodon',
+  buildType: 'single',
+  dashboard: {},
+  resourcesLevel: ['site', 'organization', 'project', 'user'],
+};
+
+module.exports = config;
