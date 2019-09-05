0b1c9cd88deb23bbdb30f1d5f0a2b56023aaa088
binjie chen <binjie.chen@hand-china.com>
2019-09-05 13:37:08 +0800

    [IMP] 优化实例管理样式，使用新的Store管理，删除无用代码

--- react/src/app/iam/containers/global/instance/list/ListView.js
+++ react/src/app/iam/containers/global/instance/list/ListView.js
@@ -1,22 +1,18 @@
 import React, { PureComponent, useState, useContext } from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import { Content, Page, Header, Breadcrumb } from '@choerodon/master';
-import { DataSet, Table, TextField, Icon, Tree } from 'choerodon-ui/pro';
+import { Icon, Tree } from 'choerodon-ui/pro';
 import { observer } from 'mobx-react-lite';
 import { Input, Button } from 'choerodon-ui';
 import DetailView from '../detail';
 import Store from './stores';
 
-import BaseTable from '../detail';
-import emptyApi from './img/noright.svg';
 import './index.less';
 
 const intlPrefix = 'global.baseTable.list';
-const { Column } = Table;
 
 export default observer((props) => {
   const { dataSet, intl } = useContext(Store);
-  const [expandedKeys, setExpandedKeys] = useState([]);
   const [code, setCode] = useState(null);
   const [inputValue, setInputValue] = useState('');
 
@@ -24,19 +20,7 @@ export default observer((props) => {
     setCode(null);
     dataSet.query();
   }
-  function findTreeNodePathByName(name) {
-    if (name) {
-      const eKeys = [];
-      dataSet.data.forEach((value) => {
-        value.data.children.forEach((tableValue) => {
-          if (tableValue.code && (tableValue.code.includes(name)) && !eKeys.includes(`db&${tableValue.databaseId}`)) {
-            eKeys.push(`db&${tableValue.databaseId}`);
-          }
-        });
-      });
-      setExpandedKeys(eKeys);
-    }
-  }
+  
   function showDetail(record) {
     if (record.get('service')) {
       setCode(record.get('instanceId'));
@@ -48,13 +32,16 @@ export default observer((props) => {
     const index = name.indexOf(searchValue);
     const beforeStr = name.substr(0, index);
     const afterStr = name.substr(index + searchValue.length);
-    const title = index > -1 ? (
+    const title = (
       <span className="tree-title" onClick={() => showDetail(record)}>
+        {!record.get('service') && <Icon type={record.get('expand') ? 'folder_open2' : 'folder_open'} />}
+        {record.get('service') && <Icon type="instance_outline" />}
+
         {beforeStr}
         <span style={{ color: '#f50' }}>{inputValue}</span>
         {afterStr}
       </span>
-    ) : <span className="tree-title" onClick={() => showDetail(record)}>{name}</span>;
+    );
     return title;
   }
   function nodeRenderer({ record }) {
@@ -66,13 +53,6 @@ export default observer((props) => {
   
   return (
     <Page>
-      <Header
-        title={<FormattedMessage id={`${intlPrefix}.header.title`} />}
-      >
-        <Button icon="refresh" onClick={refresh}>
-          <FormattedMessage id="refresh" />
-        </Button>
-      </Header>
       <Breadcrumb />
       <Content className="c7n-instance">
         <div className="c7n-instance-tree">
